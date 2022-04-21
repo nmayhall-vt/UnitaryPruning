afcc41f073fb90a48b8342561e7dd18c36c8c731
Nick Mayhall <nmayhall@vt.edu>
2022-04-21 17:02:33 -0400

    added beh2

--- src/python/beh2_2R/test1.jl
+++ src/python/beh2_2R/test1.jl
@@ -6,9 +6,8 @@ np = pyimport("numpy")
 pickle = pyimport("pickle")
 
 T = UInt
-N = 8
+N = 10 
 
-println(dir)
 
 ham_par    = Vector{Float64}()
 ansatz_par = Vector{Float64}()
@@ -17,50 +16,50 @@ ham_ops    = Vector{PauliString{N}}()
 ansatz_ops = Vector{PauliString{N}}()
 ansatz_ops_grouped = Vector{Vector{Tuple{PauliString{N}, Float64}}}()
 
-ref_state = [1,1,1,1,1,1,0,0,0,0,0,0]
+ref_state = [1,1,1,1,1,1,1,0,0,0,0,0,0,0]
 
 
-for i in np.load(dir*"/ansatz_par.npy")
+for i in np.load("ansatz_par.npy")
     # Ansatz parameters should be real 
     # openfermion tends to add phase to coefficients
     push!(ansatz_par, real(-1im * i))
 end
-for i in np.load(dir*"/ham_par.npy")
+for i in np.load("ham_par.npy")
     push!(ham_par, i)
 end
 
-function mypickle(filename, obj)
-    out = open(filename,"w")
-    pickle.dump(obj, out)
-    close(out)
- end
-
-function myunpickle(filename)
-    r = nothing
-    @pywith pybuiltin("open")(filename,"rb") as f begin
-        r = pickle.load(f)
-    end
-    return r
-end
-
-grouped = myunpickle(dir*"ansatz_ops_grouped.pkl")
-for i in grouped
-    tmp::Vector{Tuple{PauliString{N},Float64}} = []
-    for j in i
-        println(j[2], " ", abs(j[1]))
-        push!(tmp, (PauliString(j[2]), real(-1.0im*j[1])))
-    end
-    push!(ansatz_ops_grouped, tmp)
-    println()
-end
-
-for a in ansatz_ops_grouped
-    for i in a
-        for j in a
-            commute(i[1], j[1]) || error("here")
-        end
-    end
-end
+#function mypickle(filename, obj)
+#    out = open(filename,"w")
+#    pickle.dump(obj, out)
+#    close(out)
+# end
+#
+#function myunpickle(filename)
+#    r = nothing
+#    @pywith pybuiltin("open")(filename,"rb") as f begin
+#        r = pickle.load(f)
+#    end
+#    return r
+#end
+#
+#grouped = myunpickle(dir*"ansatz_ops_grouped.pkl")
+#for i in grouped
+#    tmp::Vector{Tuple{PauliString{N},Float64}} = []
+#    for j in i
+#        println(j[2], " ", abs(j[1]))
+#        push!(tmp, (PauliString(j[2]), real(-1.0im*j[1])))
+#    end
+#    push!(ansatz_ops_grouped, tmp)
+#    println()
+#end
+#
+#for a in ansatz_ops_grouped
+#    for i in a
+#        for j in a
+#            commute(i[1], j[1]) || error("here")
+#        end
+#    end
+#end
 
 do_mask = true 
 
@@ -68,26 +67,23 @@ if do_mask
     ansatz_ops = Vector{PauliMask{T,N}}()
     ham_ops    = Vector{PauliMask{T,N}}()
     ref_state = parse(T, join(ref_state); base=2)
-    for i in np.load(dir*"/ham_ops.npy")
+    for i in np.load("ham_ops.npy")
         push!(ham_ops, PauliMask(PauliString(i)))
     end
-    for i in np.load(dir*"/ansatz_ops.npy")
+    for i in np.load("ansatz_ops.npy")
         push!(ansatz_ops, PauliMask(PauliString(i)))
     end
 else
-    for i in np.load(dir*"/ham_ops.npy")
+    for i in np.load("ham_ops.npy")
         push!(ham_ops, PauliString(i))
     end
-    for i in np.load(dir*"/ansatz_ops.npy")
+    for i in np.load("ansatz_ops.npy")
         push!(ansatz_ops, PauliString(i))
     end
 end
 
-ref_val = -3.04433127
-e_hf = -2.7221671004
-
-ref_val = -7.83360160
-e_hf = -7.7393739490
+ref_val = -14.40362604
+e_hf = -14.3518804762
 
 
 
@@ -95,7 +91,7 @@ e_list = []
 e_list2 = []
 thresh_list = []
 t_list = []
-for i in 1:5
+for i in 1:9
     thresh = 10.0^(-i)
     t = @elapsed ei = UnitaryPruning.compute_expectation_value_iter(ref_state, ham_ops, ham_par, ansatz_ops, ansatz_par, thresh=thresh)
     push!(e_list, ei)
--- src/python/h6_2A/test1.jl
+++ src/python/h6_2A/test1.jl
@@ -6,9 +6,8 @@ np = pyimport("numpy")
 pickle = pyimport("pickle")
 
 T = UInt
-N = 8
+N = 12 
 
-println(dir)
 
 ham_par    = Vector{Float64}()
 ansatz_par = Vector{Float64}()
@@ -17,50 +16,50 @@ ham_ops    = Vector{PauliString{N}}()
 ansatz_ops = Vector{PauliString{N}}()
 ansatz_ops_grouped = Vector{Vector{Tuple{PauliString{N}, Float64}}}()
 
-ref_state = [1,1,1,1,1,1,0,0,0,0,0,0]
+ref_state = [1,1,1,1,1,1,1,1,0,0,0,0,0,0,0,0]
 
 
-for i in np.load(dir*"/ansatz_par.npy")
+for i in np.load("ansatz_par.npy")
     # Ansatz parameters should be real 
     # openfermion tends to add phase to coefficients
     push!(ansatz_par, real(-1im * i))
 end
-for i in np.load(dir*"/ham_par.npy")
+for i in np.load("ham_par.npy")
     push!(ham_par, i)
 end
 
-function mypickle(filename, obj)
-    out = open(filename,"w")
-    pickle.dump(obj, out)
-    close(out)
- end
-
-function myunpickle(filename)
-    r = nothing
-    @pywith pybuiltin("open")(filename,"rb") as f begin
-        r = pickle.load(f)
-    end
-    return r
-end
-
-grouped = myunpickle(dir*"ansatz_ops_grouped.pkl")
-for i in grouped
-    tmp::Vector{Tuple{PauliString{N},Float64}} = []
-    for j in i
-        println(j[2], " ", abs(j[1]))
-        push!(tmp, (PauliString(j[2]), real(-1.0im*j[1])))
-    end
-    push!(ansatz_ops_grouped, tmp)
-    println()
-end
-
-for a in ansatz_ops_grouped
-    for i in a
-        for j in a
-            commute(i[1], j[1]) || error("here")
-        end
-    end
-end
+#function mypickle(filename, obj)
+#    out = open(filename,"w")
+#    pickle.dump(obj, out)
+#    close(out)
+# end
+#
+#function myunpickle(filename)
+#    r = nothing
+#    @pywith pybuiltin("open")(filename,"rb") as f begin
+#        r = pickle.load(f)
+#    end
+#    return r
+#end
+#
+#grouped = myunpickle(dir*"ansatz_ops_grouped.pkl")
+#for i in grouped
+#    tmp::Vector{Tuple{PauliString{N},Float64}} = []
+#    for j in i
+#        println(j[2], " ", abs(j[1]))
+#        push!(tmp, (PauliString(j[2]), real(-1.0im*j[1])))
+#    end
+#    push!(ansatz_ops_grouped, tmp)
+#    println()
+#end
+#
+#for a in ansatz_ops_grouped
+#    for i in a
+#        for j in a
+#            commute(i[1], j[1]) || error("here")
+#        end
+#    end
+#end
 
 do_mask = true 
 
@@ -68,27 +67,29 @@ if do_mask
     ansatz_ops = Vector{PauliMask{T,N}}()
     ham_ops    = Vector{PauliMask{T,N}}()
     ref_state = parse(T, join(ref_state); base=2)
-    for i in np.load(dir*"/ham_ops.npy")
+    for i in np.load("ham_ops.npy")
         push!(ham_ops, PauliMask(PauliString(i)))
     end
-    for i in np.load(dir*"/ansatz_ops.npy")
+    for i in np.load("ansatz_ops.npy")
         push!(ansatz_ops, PauliMask(PauliString(i)))
     end
 else
-    for i in np.load(dir*"/ham_ops.npy")
+    for i in np.load("ham_ops.npy")
         push!(ham_ops, PauliString(i))
     end
-    for i in np.load(dir*"/ansatz_ops.npy")
+    for i in np.load("ansatz_ops.npy")
         push!(ansatz_ops, PauliString(i))
     end
 end
 
-ref_val = -3.04433127
-e_hf = -2.7221671004
+ref_val = -14.40362604
+e_hf = -14.3518804762
 
 ref_val = -7.83360160
 e_hf = -7.7393739490
 
+ref_val = -3.04433127
+e_hf = -2.7221671004
 
 
 e_list = []
