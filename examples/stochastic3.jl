a4f78f669207bada30a997443942ad81eb68c568
Nick Mayhall <nmayhall@vt.edu>
2023-07-10 12:57:09 -0400

    added print

--- examples/stochastic3.jl
+++ examples/stochastic3.jl
@@ -5,9 +5,9 @@ using Printf
 using Random
 using LinearAlgebra
 
-function run(;α=.01, k=10, nsamples=10000)
+function run(;α=.01, k=10, nsamples=10000, seed=1)
     
-    N = 40  # number of qubits
+    N = 20  # number of qubits
 
     generators = Vector{PauliBoolVec{N}}([])
     parameters = Vector{Float64}([])
@@ -41,16 +41,21 @@ function run(;α=.01, k=10, nsamples=10000)
 
     #Mz
     # o = PauliBoolVec(N, X=[13,29,31], Y=[9,30], Z=[8,12,17,28,32])
-    o = PauliBoolVec(N, Z=[1])
+    o = PauliBoolVec(N, Z=[1,2,3,4])
 
 
     ket = zeros(Bool, N)
-    Random.seed!(2)
-    results = Vector{ComplexF64}([])
-    e = 0.0
-    for i in 1:nsamples
+    Random.seed!(seed)
+    e = zeros(ComplexF64, nsamples)
+    # e2 = zeros(ComplexF64, nsamples)
+    
+    ei, _ = UnitaryPruning.stochastic_pauli_dynamics_run(generators, parameters, o, ket)
+    e[1] = ei 
+    
+    for i in 2:nsamples
         ei, _ = UnitaryPruning.stochastic_pauli_dynamics_run(generators, parameters, o, ket)
-        e += ei
+        e[i] = (e[i-1]*(i-1) + ei)/i
+        # e2 += ei*ei
     end
     # @printf(" Mean: %12.8f Stdev: %12.8f\n", mean(results), std(results))
 
@@ -59,18 +64,43 @@ function run(;α=.01, k=10, nsamples=10000)
     #     idx > 1 || continue
     #     push!(out, (out[idx-1]*(idx-1)+i)/idx)
     # end
-    e = e / nsamples
+    # e .= e ./ nsamples
 
-    return e 
+    # e2 = e2 / nsamples
+    # return e, e2 - e*e 
+    return e
 end
 
 # e = run(;α=.01, k=100, nsamples=10000)
 
+nruns = 100
+nsamples = 1000
 
-vals = [];
+e_avgs = [];
+e_vals = [];
+v_vals = [];
 for i in 0:16
-    push!(vals, run(;α=i*π/32, k=2, nsamples=100000))
-    println(vals[end])
+    e = []
+    etraj = []
+    for runi in 1:nruns
+        ei = run(α=i*π/32, k=5, nsamples=nsamples, seed=runi)
+        push!(e, real(ei[end]))
+        # push!(e, mean(real(ei)))
+        # push!(v_vals, v)
+        push!(etraj, ei)
+    end
+    push!(e_vals, etraj)
+    avg = mean(e)
+    push!(e_avgs, mean(etraj) )
+    std = sqrt(sum(e.*e)) - avg*avg
+    @printf(" avg: %12.8f var: %12.8f\n", avg, std)
+    #@printf(" avg: %12.8f %12.8fi var: %12.8f %12.8fi\n", real(avg), imag(avg), real(std), imag(std))
+    push!(v_vals, std)
+
+    plot(real(e_vals[i+1]), ylim=[-1,1], legend=false, color=:grey, alpha=.1)
+    plot!(real(e_avgs[i+1]), color=:black)
+    savefig(@sprintf "%05i.pdf" i)
 end
+
 
 # plot(vals, marker = :circle)
