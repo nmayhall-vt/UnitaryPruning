2171cc6ab723303cf4587f9fbb7ea1f699f89421
Nick Mayhall <nmayhall@vt.edu>
2023-07-11 20:59:39 -0400

    middle of change - not working

--- examples/stochastic3.jl
+++ examples/stochastic3.jl
@@ -1,5 +1,5 @@
+using Distributed
 @everywhere begin
-    using Distributed
     using UnitaryPruning
     using Plots
     using Statistics
@@ -9,7 +9,7 @@
     using SharedArrays
 end
 
-@everywhere function run(;α=.01, k=10, nsamples=10000, seed=1)
+@everywhere function get_unitary_sequence_1D(;α=.01, k=10)
     
     N = 6  # number of qubits
 
@@ -41,62 +41,72 @@ end
         end
     end
 
-    # o = PauliBoolVec(N,X=[1],Y=[2],Z=[3])
+    return generators, parameters
+end
 
-    #Mz
-    # o = PauliBoolVec(N, X=[13,29,31], Y=[9,30], Z=[8,12,17,28,32])
-    # o = PauliBoolVec(N, Z=[1,2,3,4])
-    o = PauliBoolVec(N, Y=[1], Z=[2,3,4])
-    # o = PauliBoolVec(N, Z=[1,30])
-    # o = PauliBoolVec(N, Z=[1])
-
-
-    ket = zeros(Bool, N)
+@everywhere function run(generators, parameters, o, ket, nsamples; seed=1)
     Random.seed!(seed)
-    rolling_avg = zeros(ComplexF64, nsamples)
+    rolling_avg = zeros(nsamples)
+    rolling_var = zeros(nsamples)
     
     samples = UnitaryPruning.stochastic_pauli_dynamics(generators, parameters, o, ket, nsamples=nsamples)
+    samples .= real.(samples)
 
-    rolling_avg[1] = samples[1] 
+    rolling_avg[1] = samples[1]
+    rolling_var[1] = samples[1]*samples[1]
     
     for i in 2:nsamples
         rolling_avg[i] = (rolling_avg[i-1]*(i-1) + samples[i])/i
+        rolling_var[i] = (rolling_var[i-1]*(i-1) + samples[i]*samples[i])/i
     end
-    return rolling_avg, samples 
+    return rolling_avg, rolling_var 
 end
 
 
 
 nruns = 100
-nsamples = 10000
+nsamples = 1000
 
 final_vals_stoc = []
 final_vals_errs = []
 
 # for i in 7:8
 for i in 0:16
-    avg_traj = zeros(nsamples)
-   
-    @everywhere nsamples = $nsamples
-    @everywhere trajectory = zeros(nsamples)
-    # trajectories = [zeros(nsamples) for i in nruns]
 
+    # Operator
+    # o = PauliBoolVec(N, X=[13,29,31], Y=[9,30], Z=[8,12,17,28,32])
+    # o = PauliBoolVec(N, Z=[1,2,3,4])
+    # o = PauliBoolVec(N, Y=[1], Z=[2,3,4])
+    # o = PauliBoolVec(N, Z=[1,30])
+    global o = PauliBoolVec(N, Z=[1])
+
+    # State
+    ket = zeros(Bool, N)
+    
+    
+    avg_traj = zeros(nsamples)
+    var_traj = zeros(nsamples)
+    std_traj = zeros(nsamples)
+  
+    @everywhere generators, parameters = get_unitary_sequence_1D(α=$i*π/32, k=5)
+    # @everywhere trajectory = zeros(nsamples)
+    # trajectories = [zeros(nsamples) for i in nruns]
 
     # trajectories = SharedArray{Vector{Float64}}([zeros(nsamples) for i in nruns])
     # trajectories = SharedMatrix{Float64}(nruns,nsamples)
     # avg_traj = SharedVector{Float64}(nsamples)
 
-    avg_traj = @sync @distributed (.+) for runi in 1:nruns
-    # for runi in 1:nruns
-        ei, _ = run(α=i*π/32, k=5, nsamples=nsamples, seed=runi)
-        ei .= real.(ei)
-        traj = ei
-        # trajectory .= ei
-        # trajectories[runi,:] .= ei
-        # avg_traj .+= ei
-    end
+
+    # avg_traj, var_traj = @sync @distributed (.+) for runi in 1:nruns
+    #     avg_traji, var_traji = run(generators, parameters, o, ket, nsamples, seed=runi)
+    # end
+    
+    @sync pmap(runi -> run(generators, parameters, o, ket, nsamples, seed=runi), 1:nruns)
+    
     avg_traj ./= nruns
-    var_traj = sum([(i .- avg_traj).*(i .- avg_traj) for i in trajectories]) ./nruns
+    var_traj ./= nruns
+    var_traj .= var_traj .- avg_traj.*avg_traj
+    # var_traj = sum([(i .- avg_traj).*(i .- avg_traj) for i in trajectories]) ./nruns
     
     std_traj = sqrt.(var_traj)
     z = 1.96 # 95% confidence
@@ -105,14 +115,14 @@ for i in 0:16
     @printf(" α: %4i avg: %12.8f ± %-12.6f var: %12.8f\n", i, avg_traj[end], std_traj[end], var_traj[end])
 
     # plot(trajectories, ylim=[-1,1], legend=false, color=:grey, alpha=.1)
-    # plot!(avg_traj, color=:black, ribbon=std_traj)
+    plot(avg_traj, color=:black, ribbon=std_traj, ylim=[-1,1], legend=false)
     # plot!(avg_traj .+ std_traj, color=:red)
     # plot!(avg_traj .- std_traj, color=:red)
-    # savefig(@sprintf "%05i.pdf" i)
+    savefig(@sprintf "%05i.pdf" i)
     push!(final_vals_stoc, avg_traj[end])
     push!(final_vals_errs, std_traj[end])
 end
 
-# plot(final_vals_stoc, ribbon=final_vals_errs, marker=:circle); 
-# plot!(final_vals); 
-# savefig("plot.pdf")
+plot(final_vals_stoc, ribbon=final_vals_errs, marker=:circle); 
+plot!(final_vals); 
+savefig("plot.pdf")
