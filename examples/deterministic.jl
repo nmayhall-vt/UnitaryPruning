c331de6d5099c8492a77433cb044741c3cd32083
Nick Mayhall <nmayhall@vt.edu>
2023-08-24 21:05:36 -0400

    made function type stable

--- examples/deterministic.jl
+++ examples/deterministic.jl
@@ -40,56 +40,6 @@ end
     return generators, parameters
 end
 
-# @everywhere function get_unitary_sequence_1D(o::PauliBoolVec{N}; α=.01, k=10) where N
-   
-#     generators = Vector{PauliBoolVec{N}}([])
-#     parameters = Vector{Float64}([])
-
-#     # Loop over trotter steps
-#     for ki in 1:k
-        
-#         ## ZZ layer
-#         # e^{i π/4 P2} e^{i π P1 /4}|ψ>
-#         for i in 1:N-1
-#             pi = PauliBoolVec(N, Z=[i, i + 1])
-#             push!(generators, pi)
-#             push!(parameters, π/2)
-#         end
-            
-#         #pbc 
-#         pi = PauliBoolVec(N, Z=[N, 1])
-#         push!(generators, pi)
-#         push!(parameters, π/2 )
-
-#         ## X layer
-#         # e^{i αn Pn / 2}
-#         for i in 1:N
-#             pi = PauliBoolVec(N, X=[i])
-#             pi += 2 # this accounts for the fact that the papers have -X and positive ZZ
-#             push!(generators, pi)
-#             push!(parameters, α)
-#         end
-#     end
-
-#     return generators, parameters
-# end
-
-@everywhere function compute_run(generators, parameters, o, ket, N)
-#    Random.seed!(seed)
-#    rolling_avg = zeros(nsamples)
-
-    samples = UnitaryPruning.deterministic_pauli_rotations(generators, parameters, o, ket, N)
-
-    samples .= real.(samples)
-
-    rolling_avg[1] = samples[1]
-    
-    for i in 2:nsamples
-        rolling_avg[i] = (rolling_avg[i-1]*(i-1) + samples[i])/i
-    end
-    return rolling_avg, rolling_avg.*rolling_avg 
-end
-
 
 function run(; N=6)
    
@@ -114,19 +64,21 @@ function run(; N=6)
     final_vals_stoc = []
     final_vals_errs = []
 
-    for i in [(i-1)*2 for i in 1:16]
+    # for i in [(i-1)*2 for i in 1:16]
+    for i in 8:8 
 
         
         #
         # Uncomment the following to do a serial run
         #
-        generators, parameters = get_unitary_sequence_1D(o, α=i * π / 32, k=2)
+        # generators, parameters = get_unitary_sequence_1D(o, α=i * π / 32, k=2)
+        generators, parameters = UnitaryPruning.eagle_processor(o, α=i * π / 32, k=4)
         # for g in generators
         #     print(g)
         # end
         # a, b = compute_run(generators, parameters, o, ket, N)
     
-        e = UnitaryPruning.deterministic_pauli_rotations(generators, parameters, o, ket, N)
+        e = UnitaryPruning.deterministic_pauli_rotations(generators, parameters, o, ket, thres=1e-3)
 
 
         @printf(" α: %4i e: %12.8f+%12.8fi\n", i, real(e), imag(e))
@@ -139,4 +91,4 @@ function run(; N=6)
     return final_vals_stoc, final_vals_errs
 end
 
-@time v,e = run(N=10)
+@time v,e = run(N=127)
--- src/deterministic_evolution.jl
+++ src/deterministic_evolution.jl
@@ -1,26 +1,27 @@
 using Random
-
+using BenchmarkTools
 """
     deterministic_pauli_rotations(generators::Vector{P}, angles, o::P, ket; nsamples=1000) where {N, P<:Pauli}
 
 
 """
-function deterministic_pauli_rotations(generators::Vector{P}, angles, o::P, ket, N) where {P<:Pauli}
+function deterministic_pauli_rotations(generators::Vector{PauliBitString{N}}, angles, o::PauliBitString{N}, ket ; thres=1e-3) where {N}
 
     #
     # for a single pauli Unitary, U = exp(-i θn Pn/2)
     # U' O U = cos(θ) O + i sin(θ) OP
-    thres = -10^-3
     nt = length(angles)
     length(angles) == nt || throw(DimensionMismatch)
-#    print("nt", nt)
+    
+    vcos = cos.(angles)
+    vsin = sin.(angles)
 
     # collect our results here...
     expval = zero(ComplexF64)
 
 
     # Loop through the generators in reverse, i.e., from the operator to the state
-    opers = Dict((o.z,o.x)=>1.0*(1im)^o.θ)
+    opers = Dict{Tuple{Int128, Int128}, Complex{Float64}}((o.z,o.x)=>1.0*(1im)^o.θ)
         
     
     for t in reverse(1:nt)
@@ -28,10 +29,10 @@ function deterministic_pauli_rotations(generators::Vector{P}, angles, o::P, ket,
         g = generators[t]
         branch_opers = Dict{Tuple{Int128, Int128}, Complex{Float64}}()
 
+        sizehint!(branch_opers, 1000)
         for (key,value) in opers
             
             oi = PauliBitString{N}(0,key[1],key[2])
-
             if commute(oi, g)
                 if haskey(branch_opers, (oi.z,oi.x))
                     branch_opers[(oi.z, oi.x)] += (1im)^oi.θ * value
@@ -42,7 +43,7 @@ function deterministic_pauli_rotations(generators::Vector{P}, angles, o::P, ket,
             end
 
             if abs(value) > thres #If greater than threshold then split the branches
-                coeff = cos(angles[t]) * value
+                coeff = vcos[t] * value
             
                
                 # cos branch
@@ -53,7 +54,7 @@ function deterministic_pauli_rotations(generators::Vector{P}, angles, o::P, ket,
                 end
 
                 # sin branch
-                coeff = sin(angles[t]) * 1im * value
+                coeff = vsin[t] * 1im * value
                 oi = multiply(g, oi)    # multiply the pauli's
 
                 if !haskey(branch_opers, (oi.z, oi.x)) # Add operator to dictionary if the key doesn't exist
@@ -61,8 +62,6 @@ function deterministic_pauli_rotations(generators::Vector{P}, angles, o::P, ket,
                 else
                     branch_opers[(oi.z, oi.x)] += coeff * (1im)^oi.θ #Modify the coeff if the key exists already
                 end
-            else
-                throw(ErrorException)
             end
         end
         
